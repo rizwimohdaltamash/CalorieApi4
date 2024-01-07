e1a927f34507f8fffb20e44c751c2581a35e1e5b
aialok <alokgupta1560@gmail.com>
2024-01-07 08:52:05 +0530

    Refactor code

---
+++ CalorieAPI.js
@@ -0,0 +1,296 @@
+const data = [
+  {
+    name: "Roti",
+    calories: 80,
+  },
+  {
+    name: "ChickenFriedRice",
+    calories: 350,
+  },
+  {
+    name: "MatarPulao",
+    calories: 250,
+  },
+  {
+    name: "AlooMethi",
+    calories: 180,
+  },
+  {
+    name: "EggCurry",
+    calories: 300,
+  },
+  {
+    name: "CabbageSabzi",
+    calories: 120,
+  },
+  {
+    name: "DalTadka",
+    calories: 200,
+  },
+  {
+    name: "JeeraRice",
+    calories: 150,
+  },
+  {
+    name: "FishFry",
+    calories: 250,
+  },
+  {
+    name: "AlooBaingan",
+    calories: 160,
+  },
+  {
+    name: "Chapati",
+    calories: 70,
+  },
+  {
+    name: "MixedVegetableCurry",
+    calories: 220,
+  },
+  {
+    name: "CucumberRaita",
+    calories: 90,
+  },
+  {
+    name: "MushroomMasala",
+    calories: 280,
+  },
+  {
+    name: "TofuBhurji",
+    calories: 200,
+  },
+  {
+    name: "PalakKhichdi",
+    calories: 230,
+  },
+  {
+    name: "GobiManchurian",
+    calories: 300,
+  },
+  {
+    name: "VegetableSambar",
+    calories: 180,
+  },
+  {
+    name: "MasoorDal",
+    calories: 160,
+  },
+  {
+    name: "EggBhurji",
+    calories: 220,
+  },
+  {
+    name: "LaukiKofta",
+    calories: 250,
+  },
+  {
+    name: "PumpkinSabzi",
+    calories: 140,
+  },
+  {
+    name: "ChickenKebabs",
+    calories: 280,
+  },
+  {
+    name: "TomatoRasam",
+    calories: 100,
+  },
+  {
+    name: "CapsicumRice",
+    calories: 200,
+  },
+  {
+    name: "Biryani",
+    calories: 350,
+  },
+  {
+    name: "ButterChicken",
+    calories: 450,
+  },
+  {
+    name: "PaneerTikka",
+    calories: 200,
+  },
+  {
+    name: "MasalaDosa",
+    calories: 300,
+  },
+  {
+    name: "CholeBhature",
+    calories: 600,
+  },
+  {
+    name: "TandooriRoti",
+    calories: 150,
+  },
+  {
+    name: "Samosa",
+    calories: 200,
+  },
+  {
+    name: "AlooGobi",
+    calories: 250,
+  },
+  {
+    name: "PalakPaneer",
+    calories: 300,
+  },
+  {
+    name: "DalMakhani",
+    calories: 350,
+  },
+  {
+    name: "Rasgulla",
+    calories: 100,
+  },
+  {
+    name: "GulabJamun",
+    calories: 150,
+  },
+  {
+    name: "PavBhaji",
+    calories: 400,
+  },
+  {
+    name: "RajmaChawal",
+    calories: 350,
+  },
+  {
+    name: "KadaiChicken",
+    calories: 500,
+  },
+  {
+    name: "AlooParatha",
+    calories: 300,
+  },
+  {
+    name: "ChickenBiryani",
+    calories: 400,
+  },
+  {
+    name: "MangoLassi",
+    calories: 200,
+  },
+  {
+    name: "PaniPuri",
+    calories: 100,
+  },
+  {
+    name: "Pulao",
+    calories: 350,
+  },
+  {
+    name: "ChickenCurry",
+    calories: 450,
+  },
+  {
+    name: "IdliSambar",
+    calories: 250,
+  },
+  {
+    name: "Jalebi",
+    calories: 300,
+  },
+  {
+    name: "ChickenKorma",
+    calories: 400,
+  },
+  {
+    name: "VegetableBiryani",
+    calories: 300,
+  },
+  {
+    name: "Dhokla",
+    calories: 150,
+  },
+  {
+    name: "AlooTikki",
+    calories: 180,
+  },
+  {
+    name: "FishCurry",
+    calories: 350,
+  },
+  {
+    name: "MuttonRoganJosh",
+    calories: 500,
+  },
+  {
+    name: "Pesarattu",
+    calories: 200,
+  },
+  {
+    name: "Kheer",
+    calories: 250,
+  },
+  {
+    name: "BainganBharta",
+    calories: 220,
+  },
+  {
+    name: "ChickenTikka Masala",
+    calories: 400,
+  },
+  {
+    name: "PapdiChaat",
+    calories: 120,
+  },
+  {
+    name: "MysorePak",
+    calories: 180,
+  },
+  {
+    name: "ChickenSaagwala",
+    calories: 420,
+  },
+  {
+    name: "AlooMatar",
+    calories: 230,
+  },
+  {
+    name: "FishBiryani",
+    calories: 380,
+  },
+  {
+    name: "BhindiMasala",
+    calories: 200,
+  },
+  {
+    name: "ChanaMasala",
+    calories: 300,
+  },
+  {
+    name: "KoftaCurry",
+    calories: 350,
+  },
+  {
+    name: "Raita",
+    calories: 80,
+  },
+  {
+    name: "KeemaParatha",
+    calories: 320,
+  },
+  {
+    name: "CoconutChutney",
+    calories: 90,
+  },
+  {
+    name: "ShahiTukda",
+    calories: 250,
+  },
+  {
+    name: "PrawnCurry",
+    calories: 400,
+  },
+  {
+    name: "VegetableKorma",
+    calories: 280,
+  },
+  {
+    name: "Chicken65",
+    calories: 300,
+  },
+];
+
+module.exports = data;
